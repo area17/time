3b3975f683b5ce3cef6c4b913d194a25adb83542
Mike Byrne <mike@thirteentwelve.com>
2015-01-13 16:09:43 +0000

    Updated proxy.php to remove my Forecast api key and replace it with instructions on how to get an API key

--- proxy.php
+++ proxy.php
@@ -1,7 +1,7 @@
 <?php
 // File Name: proxy.php
-
-$api_key = '545ef329c18ffc30131b99714ec182ca';
+// get a Forecast.io key here: https://developer.forecast.io/
+$api_key = 'forecast api key';
 
 $API_ENDPOINT = 'https://api.forecast.io/forecast/';
 $url = $API_ENDPOINT . $api_key . '/';
