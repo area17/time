58a369065a8ab89526b68e644649901ba8f34812
Mike Byrne <mike@thirteentwelve.com>
2017-01-09 11:42:36 +0000

    Corrected Manchester's longnitude

--- includes/_timezones_setup.php
+++ includes/_timezones_setup.php
@@ -10,7 +10,7 @@ $hourEmoji = array("0" => "🕛", "1" => "🕐", "2" => "🕑", "3" => "🕒", "
 
 // our locations
 array_push($locations, array("name" => "Paris", "timezone" => "Europe/Paris", "emoji" => "🇫🇷", "lat" => 48.866667, "long" => 2.333333, "offset" => 0));
-array_push($locations, array("name" => "Manchester", "timezone" => "Europe/London", "emoji" => "🇬🇧", "lat" => 53.4808, "long" => 2.2426, "offset" => 0));
+array_push($locations, array("name" => "Manchester", "timezone" => "Europe/London", "emoji" => "🇬🇧", "lat" => 53.4808, "long" => -2.2426, "offset" => 0));
 array_push($locations, array("name" => "Tucumán", "timezone" => "America/Argentina/Tucuman", "emoji" => "🇦🇷", "lat" => -26.8166667, "long" => -65.2166667, "offset" => 0));
 array_push($locations, array("name" => "New York", "timezone" => "America/New_York", "emoji" => "🇺🇸", "lat" => 40.7141667, "long" => -74.0063889, "offset" => 0));
 array_push($locations, array("name" => "Memphis", "timezone" => "America/Chicago", "emoji" => "🇺🇸", "lat" => 35.6331, "long" => -88.8208, "offset" => 0));
