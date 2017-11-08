2c7fa5481bce666dbf0b28129de90e8414657e9a
Mike Byrne <mike@thirteentwelve.com>
2017-11-08 09:15:06 +0000

    Added gtag code from George

--- index.php
+++ index.php
@@ -8,6 +8,13 @@
 ?>
 <html dir="ltr" lang="en-US">
   <head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-180083-11"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-180083-11');
+    </script>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo $meta_title; ?></title>
